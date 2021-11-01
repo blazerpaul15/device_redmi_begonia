3d4b660190d28b464e467c7de94abdb45cde3854
SebaUbuntu <barezzisebastiano@gmail.com>
2021-11-01 10:28:22 +0100

    begonia: Uprev clearkey DRM HIDL to 1.4
    Change-Id: I45bd0d7cfc84a15bfe83f5ea2c9cb4f0e7c90bdc

--- device.mk
+++ device.mk
@@ -86,6 +86,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.1.vendor \
     android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3.vendor \
+    android.hardware.drm@1.4.vendor \
     android.hardware.drm@1.0-impl:64 \
     android.hardware.drm@1.0-service-lazy \
     android.hardware.drm@1.4-service.clearkey
