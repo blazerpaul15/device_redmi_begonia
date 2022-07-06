4ac4b51ac1c614cddddd18cf033ee391f79e8b51
blazerpaul15 <blazerpaul15@gmail.com>
2022-07-06 19:40:46 +0530

    begonia: Update blobs from V12.5.8.0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -128,7 +128,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2022-01-01
+VENDOR_SECURITY_PATCH := 2022-04-01
 
 # Sepolicy
 include device/mediatek/sepolicy/SEPolicy.mk
