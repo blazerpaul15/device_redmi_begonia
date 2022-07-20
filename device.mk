5895b67abe2e9b446ac7b548b6aed9fb342f459f
bengris32 <bengris32@protonmail.ch>
2022-07-20 23:37:58 +0530

    begonia: Introduce MtkInCallService
    * My attempt at fixing in call gain control on MediaTek devices.
    * It attempts to replicate what the stock MediaTek framework does
      when controlling the volume of the earpiece speaker, since MTK's
      audio HAL is not able to set the gain of the speaker from the
      values sent by AOSP framework.
    
    Change-Id: I43f727f6fb73a0d38457a41a8361653d1d00e220

--- device.mk
+++ device.mk
@@ -174,6 +174,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# MtkInCallService
+PRODUCT_PACKAGES += \
+    MtkInCallService
+
 # IMS
 include vendor/redmi/begonia-ims/begonia-ims-vendor.mk
 
